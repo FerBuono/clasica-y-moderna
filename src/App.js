1fd4416c938208cca97551575d4b9cb5864189cc
Fernando <fbuono@fi.uba.ar>
2022-01-20 14:13:09 -0300

    docs actualizado

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
       classes={{variantSuccess: classes.success}}
     >
       <WrapperContext>
-            <HashRouter>
+            <HashRouter basename='/clasica-y-moderna'>
               <Navbar />
               <Routes>
                 <Route exact path="/" element={<Home />} />
